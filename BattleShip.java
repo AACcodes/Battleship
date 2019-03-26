842e8434c30140b88e4e9f466f31e00e5a321473
Ananya Gopalan <ananya.gopalan@gmail.com>
2019-03-26 08:28:40 -0400

    dan

--- BattleShip.java
+++ BattleShip.java
@@ -1,13 +1,60 @@
-//Ayush Chaudhary and Ananya Srinivasangopalan
-//AP COMPSCI
-//Project Name
-//date
+// Ayush Chaudhary and Ananya Gopalan
+// AP COMPSCI A
+// Battleship 
+// 3/26/19
 
 
 public class BattleShip //change name of program 
 {
    public static void main(String args[])
    {
+   
+              
+      System.out.println("                                          _________________________                            ");
+      System.out.println("                                         |_________________________|             _____________________               ");
+      System.out.println("                                      ___|_________________________|___         /                     \\   ");
+      System.out.println("                                     |_________________________________|        |   I'm Dan the Dino  |                  ");
+      System.out.println("                                     |______|   |______________________|        |        AND...       |             " );
+      System.out.println("                                     |______|___|______________________|        |    We're going to   |                ");
+      System.out.println("                                     |_________________________________|        |                     |                  ");
+      System.out.println("                                     |_________________________________|        |                     |                      ");
+      System.out.println("                                     |_________________________________|       /______________________|                              ");
+      System.out.println("                                     |_________________________________|                                       ");
+      System.out.println("                                     |_________________________________|                                        ");
+      System.out.println("                                     |________________|                                                          ");
+      System.out.println("                                     |________________|__________                                                ");
+      System.out.println("                                     |___________________________|                                                ");
+      System.out.println(" __                               ___|___________________________|                                              ");
+      System.out.println("|__|                             |_____________|");
+      System.out.println("|__|                         ____|_____________|");
+      System.out.println("|__|                        |__________________|");
+      System.out.println("|__|__                 _____|__________________|______");
+      System.out.println("|_____|               |_______________________________|");
+      System.out.println("|_____|___         ___|_______________________________|        ");
+      System.out.println("|_________|       |____________________________|  |___|    ");
+      System.out.println("|_________|_______|____________________________|  |___|           ");
+      System.out.println("|______________________________________________|                     ");
+      System.out.println("|______________________________________________|                       ");
+      System.out.println("|______________________________________________|                       ");
+      System.out.println("|______________________________________________|                       ");
+      System.out.println("|______________________________________________|                       ");
+      System.out.println("   |________________________________________|                       ");
+      System.out.println("   |________________________________________|                       ");
+      System.out.println("      |__________________________________|                       ");
+      System.out.println("      |__________________________________|                       ");
+      System.out.println("          |_________________________|                       ");
+      System.out.println("          |_________________________|                       "); 
+      System.out.println("              |___________|   |_____|              ");
+      System.out.println("              |___________|   |_____|              ");
+      System.out.println("              |_______|          |__|          ");
+      System.out.println("              |_______|          |  |          ");
+      System.out.println("              |__|               |__|      ");
+      System.out.println("              |__|____           |__|___         ");
+      System.out.println("              |_______|          |______|      ");
+      
+      System.out.println("");
+      System.out.println("");
+
       System.out.println("Hello World");
    }
 }
