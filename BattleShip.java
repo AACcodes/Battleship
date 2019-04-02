ccfd063e5f3fbe540e9748f9363a94eca68f6621
Ananya Gopalan <ananya.gopalan@gmail.com>
2019-04-01 23:03:01 -0400

    added a message to the main file

--- BattleShip.java
+++ BattleShip.java
@@ -15,4 +15,44 @@ public class BattleShip
 }
 
  
+ /* so here's what I did...
+ 
+ I created 3 ships for the first of the 10 games.
+ I can finish that later I just forgot the nunmber of ships and their respective lengths.
+ Everything spot on the board is false. 
+ The positions where the ships are located are true.
+ 
+ you can uncomment line 57 in the file Board2.java to see the positions of the ships 
+ when you do however, make sure you comment out line 58, which is the final output of the board 
+
+What needs to be done:
+   1. create row and column numbers on our board 
+   2. create ten games similar to the one already created 
+      - this should probablly be done in another file (each different game is in its own file)
+   3. create a random generator that chooses which of the ten games you play 
+   
+If we have time:
+   1. have a quit option 
+   2. write out the direction to play the game (fairly easy --> using print statments)
+   3. have a play again option. 
+   
+   
+I can finish the first game sinareo (I spelled that wrong but idk how to fix it) I started 
+and create 4 more tomorrow, if you want to do five as well
+
+
+happy coding and text me with updates/question!! ;)
+
+Know I'm working on your brownies as a treat for all your hard work
+
+yours truley, (I might have spelled that wrong as well oops....)
+
+Ananya :)
+
+*/
+
+
+
+ 
+ 
  
