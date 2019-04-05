4f7728c50d0d0f531d72d9b0eba076f7ac9d6077
Ayush Chaudhary <ayushc.pro@gmail.com>
2019-04-05 10:52:05 -0400

    Ananya did stuff

--- BattleShip.java
+++ BattleShip.java
@@ -19,25 +19,4 @@ public class BattleShip
     
 
 }
-
- 
- /* so here's what I did...
- 
- I pretty much finished the game.... it fully works now
- 
- IMPORTANT: sorry but i didn't use your Board2 class... all important stuff is in the BattleshipBoard class so please take a look at that
- 
- all we need to do now is
- -Make it fancier
- -decide how the 10 computer boards will look (programming is as simple as selecting the diagram)
- -Test!!!!!
- 
- Looking forward to those brownies,
--Ayush C
-*/
-
-
-
- 
- 
  
